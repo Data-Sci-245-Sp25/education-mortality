e1061b5ace093e2552c18cfbad57c05db7161faa
mlestrand <sylime333@gmail.com>
2025-03-30 22:56:01 +0000

    made more draft visualizations

--- src/visualization_drafts.R
+++ src/visualization_drafts.R
@@ -18,7 +18,41 @@ ggplot(data=educCancerRatesOverall, mapping=aes(x=ddeduc, y = total_deaths, fill
 # try again with rates
 educCancerRatesOverall <- educCancerRates |>
   group_by(category, ddeduc) |>
-  summarize(total_death_rate = sum(rate_per_100k, na.rm = TRUE)/10) |>
+  summarize(total_death_rate = sum(rate_per_100k, na.rm = TRUE)/10) |> 
   ungroup()
 ggplot(data=educCancerRatesOverall, mapping=aes(x=ddeduc, y = total_death_rate, fill=ddeduc)) +
   geom_col() + facet_wrap(~category) 
+
+# ignoring cancer types
+ggplot(data=educCancerRatesOverall, mapping=aes(x=ddeduc, y = (total_death_rate/5), fill=ddeduc)) +
+  geom_col() # level 3 has the most death rates
+
+# top cancers per year 
+cancerRatesOverall <- educCancerRates |>
+  group_by(category, ddodyear) |>
+  summarize(total_death_rate = sum(rate_per_100k, na.rm = TRUE)/9) |> 
+  arrange(desc(total_death_rate))|>
+  ungroup()
+ggplot(data=cancerRatesOverall, mapping=aes(x=ddodyear, y = total_death_rate, fill=category)) +
+  geom_col(position="dodge")  # maybe try reording bars to make it easier to see
+
+# top cancers per year line chart 
+ggplot(data=cancerRatesOverall, mapping=aes(x=ddodyear, y=total_death_rate, color=category))+
+  geom_line()
+
+# scatter plot/correlation between education and death rate
+educRatesOverall <- educCancerRates |>
+  group_by(ddeduc) |>
+  summarize(total_death_rate = sum(rate_per_100k, na.rm = TRUE)/50) |> 
+  arrange(desc(total_death_rate))|>
+  ungroup()
+educRatesOverall$ddeduc <- as.numeric(educRatesOverall$ddeduc)
+ggplot(data=educRatesOverall, mapping=aes(x=ddeduc, y=total_death_rate))+
+  geom_point() + geom_smooth(method="lm")
+# correlation separated by cancer types
+educCancerRatesOverall$ddeduc <- as.numeric(educCancerRatesOverall$ddeduc)
+ggplot(data=educCancerRatesOverall, mapping=aes(x=ddeduc, y=total_death_rate, color=category))+
+  geom_point() + geom_line()
+
+# MAPS (may need to re-merge data with tidycensus...)
+ggplot() + geom_sf(data= educCancerRates, aes(fill=educCancerRates$rate_per_100k))
