3a74f20b4135fd44d4109e6e02b5aa1be87a8f30
mlestrand <sylime333@gmail.com>
2025-04-14 04:40:34 +0000

    added all draft visualizations

--- src/visualization_drafts.R
+++ src/visualization_drafts.R
@@ -63,46 +63,50 @@ educCancerRatesOverall <- educCancerRates |>
   group_by(category, ddeduc) |>
   summarize(total_death_rate = sum(rate_per_100k, na.rm = TRUE)/10) |> 
   ungroup()
+
+educCancerRatesOverall$ddeduc <- as.factor(educCancerRatesOverall$dd)
 ggplot(data=educCancerRatesOverall, mapping=aes(x=ddeduc, y = total_death_rate, fill=ddeduc)) +
   geom_col() + facet_wrap(~category) + labs(title= "Cancer Rates by Education", x="Education Level", y="Death Rate Per 100k People") +
   theme_minimal() + labs(color = "Education Level") + scale_fill_manual(values = c(
-    "#D35400",  # Dark orange
-    "#C7642C",
-    "#B67B4A",
-    "#A39268",
-    "#8DA0A0",
-    "#6F8AB3",
-    "#4B6CA6",
-    "#3B4C75"   # Lighter navy
-  ), labels=c("1: 8th Grade or Less", "2: HS (no diploma)", "3: HS Diploma/GED", "4: Some College",
-              "5: Associates", "6: Bachelor's", "7: Masters", "8: Doctorate/Professional")) + 
+    "#D35400",  # Burnt orange
+    "#C77938",  # Copper
+    "#B4985C",  # Khaki brown
+    "#9FA58D",  # Desaturated olive
+    "#7E94A9",  # Dusty blue
+    "#5D7EB2",  # Blue-gray
+    "#3F5FA3",  # Rich blue
+    "#2C3E50"   # Navy  
+  ), labels=c("1: 8th Grade or Less", "2: Some HS (No Degree)", "3: HS Diploma/GED", "4: Some College (No Degree)",
+              "5: Associates", "6: Bachelor's", "7: Master's", "8: Doctorate/Professional")) + 
   theme(
     plot.title = element_text(
       hjust = 0.5,     # 0 = left, 0.5 = center, 1 = right
       size = 15,       # Font size
-      face = "bold"    # Optional: makes it bold
+      face = "bold",    # Optional: makes it bold
+      family = "Lato"
     )
   )
 
 # ignoring cancer types
 ggplot(data=educCancerRatesOverall, mapping=aes(x=ddeduc, y = (total_death_rate), fill=ddeduc)) +
-  geom_col()+ labs(title= "Total Cancer Rates by Education", x="Education Level", y="Total Death Rate Per 100k People") +
+  geom_col()+ labs(title= "Total Cancer Death Rates by Education", x="Education Level", y="Total Death Rate Per 100k People") +
   theme_minimal() + labs(color = "Education Level") + scale_fill_manual(values = c(
-    "#E69F00",  # orange
-    "#56B4E9",  # sky blue
-    "#009E73",  # teal
-    "#F0E442",  # yellow
-    "#0072B2",  # blue
-    "#D55E00",  # reddish orange
-    "#CC79A7",  # pink
-    "#999999"   # gray
-  ), labels=c("1: 8th Grade or Less", "2: HS (no diploma)", "3: HS Diploma/GED", "4: Some College",
-              "5: Associates", "6: Bachelor's", "7: Masters", "8: Doctorate/Professional")) + 
+    "#D35400",  # Burnt orange
+    "#C77938",  # Copper
+    "#B4985C",  # Khaki brown
+    "#9FA58D",  # Desaturated olive
+    "#7E94A9",  # Dusty blue
+    "#5D7EB2",  # Blue-gray
+    "#3F5FA3",  # Rich blue
+    "#2C3E50"   # Navy
+  ), labels=c("1: 8th Grade or Less", "2: Some HS (No Degree)", "3: HS Diploma/GED", "4: Some College (No Degree)",
+              "5: Associates", "6: Bachelor's", "7: Master's", "8: Doctorate/Professional")) + 
   theme(
     plot.title = element_text(
       hjust = 0.5,     # 0 = left, 0.5 = center, 1 = right
       size = 15,       # Font size
-      #face = "bold"    # Optional: makes it bold
+      face = "bold",    # Optional: makes it bold
+      family = "Lato"
     )
   ) # level 3 has the most death rates
 
@@ -117,6 +121,7 @@ cancerRatesOverall <- cancerRatesOverall |>
   group_by(ddodyear) |>
   mutate(category_ordered = fct_reorder2(category, ddodyear, total_death_rate))
 
+# don't really need this bc we have line chart:
 ggplot(data=cancerRatesOverall, mapping=aes(x=ddodyear, y = total_death_rate, fill=category_ordered)) +
   geom_col(position="dodge")+ labs(title= "Top Cancers Over Time", x="Year", y="Total Death Rate Per 100k People") +
   theme_minimal() + labs(fill = "Disease Site")+ scale_fill_manual(values = c(
@@ -131,13 +136,14 @@ ggplot(data=cancerRatesOverall, mapping=aes(x=ddodyear, y = total_death_rate, fi
     plot.title = element_text(
       hjust = 0.5,     # 0 = left, 0.5 = center, 1 = right
       size = 15,       # Font size
-      #face = "bold"    # Optional: makes it bold
+      face = "bold",    # Optional: makes it bold
+      family = "Lato"
     )
-  )  # maybe try reording bars to make it easier to see
+  )  
 
 # top cancers per year line chart 
 ggplot(data=cancerRatesOverall, mapping=aes(x=ddodyear, y=total_death_rate, color=category))+
-  geom_line()+ labs(title= "Top Cancers Over Time", x="Year", y="Total Death Rate Per 100k People") +
+  geom_point() +geom_line()+ labs(title= "Top Cancers Over Time", x="Year", y="Total Death Rate Per 100k People") +
   theme_minimal() + labs(fill = "Disease Site")+ scale_color_manual(values = c(
     "#4e5d6c",
     "#5cb85c",
@@ -150,7 +156,8 @@ ggplot(data=cancerRatesOverall, mapping=aes(x=ddodyear, y=total_death_rate, colo
     plot.title = element_text(
       hjust = 0.5,     # 0 = left, 0.5 = center, 1 = right
       size = 15,       # Font size
-      #face = "bold"    # Optional: makes it bold
+      face = "bold",    # Optional: makes it bold
+      family = "Lato"
     )
   ) 
 
@@ -167,17 +174,41 @@ ggplot(data=educRatesOverall, mapping=aes(x=ddeduc, y=total_death_rate))+
     plot.title = element_text(
       hjust = 0.5,     # 0 = left, 0.5 = center, 1 = right
       size = 15,       # Font size
-      #face = "bold"    # Optional: makes it bold
+      face = "bold",    # Optional: makes it bold
+      family = "Lato"
     ))
+
+# Fit the model
+model <- lm(total_death_rate ~ ddeduc, data = educRatesOverall)
+# View the summary
+summary(model) # p-value is .03624: at alpha=.05 this is significant
+confint(model)
+
+
 # to do: correlation separated by cancer types
 educCancerRatesOverall$ddeduc <- as.numeric(educCancerRatesOverall$ddeduc)
+
+r2_labels <- educCancerRatesOverall |>
+  group_by(category) |>
+  do({
+    model <- lm(total_death_rate ~ ddeduc, data = .)
+    r2 <- summary(model)$r.squared
+    tibble(
+      category = unique(.$category),
+      label = paste0("R² = ", round(r2, 2)),
+      x = max(.$ddeduc),      # position for label on x-axis
+      y = predict(model, newdata = data.frame(ddeduc = max(.$ddeduc)))  # y value from model
+    )
+  })
+
 ggplot(data=educCancerRatesOverall, mapping=aes(x=ddeduc, y=total_death_rate, color=category))+
-  geom_point() + geom_line()+ labs(title="Education Level vs Death Rate by Disease Site", x="Education Level", y="Avg Death Rate Per 100k")+
+  geom_point() + geom_smooth(method="lm", se=FALSE) + labs(title="Education Level vs Death Rate by Disease Site", x="Education Level", y="Avg Death Rate Per 100k")+
   theme(
     plot.title = element_text(
       hjust = 0.5,     # 0 = left, 0.5 = center, 1 = right
       size = 15,       # Font size
-      #face = "bold"    # Optional: makes it bold
+      face = "bold",    # Optional: makes it bold
+      family = "Lato"
     ))+ labs(color = "Disease Site")+ scale_color_manual(values = c(
       "#4e5d6c",
       "#5cb85c",
@@ -185,10 +216,40 @@ ggplot(data=educCancerRatesOverall, mapping=aes(x=ddeduc, y=total_death_rate, co
       "#d9534f",
       "#ffc107"  
     ), labels=c("Breast", "Colon", "Lung", "Lymphatic or Blood",
-                "Pancreas"))
-#+
-  #transition_reveal(ddeduc)
-#anim_save("287-smooth-animation-with-tweenr.gif")
+                "Pancreas"))+
+  geom_text(data = r2_labels, aes(x = x, y = y, label = label, color = category),
+            hjust = 0.15, vjust = -0.5, size = 3.5, show.legend = FALSE)
+
+# Fit the model
+model <- lm(total_death_rate ~ ddeduc, data = educCancerRatesOverall)
+# View the summary
+summary(model)
+
+# LUNG: R^2=.601, p-value=.02381
+model <- educCancerRatesOverall |>
+  subset(category == "Lung") |>
+  lm(total_death_rate ~ ddeduc, data = _)
+summary(model)
+# BREAST: R^2=.1195, p-value=.4017
+model <- educCancerRatesOverall |>
+  subset(category == "Breast") |>
+  lm(total_death_rate ~ ddeduc, data = _)
+summary(model)
+# COLON: R^2=.5399, p-value=.03786
+model <- educCancerRatesOverall |>
+  subset(category == "Colon") |>
+  lm(total_death_rate ~ ddeduc, data = _)
+summary(model)
+# LYMPHATIC OR BLOOD: R^2=.6498, p-value=.01568
+model <- educCancerRatesOverall |>
+  subset(category == "Lymphatic or Blood") |>
+  lm(total_death_rate ~ ddeduc, data = _)
+summary(model)
+# PANCREAS: R^2=.5734, p-value=.02958
+model <- educCancerRatesOverall |>
+  subset(category == "Pancreas") |>
+  lm(total_death_rate ~ ddeduc, data = _)
+summary(model)
 
 # MAPS (may need to re-merge data with tidycensus...)
 #ggplot() + geom_sf(data= educCancerRates, aes(fill=educCancerRates$rate_per_100k))
